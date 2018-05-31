e1e251040cfc2618a18822bf29c2b313a4d7726c
hecpalomares <hpalomares12@gmail.com>
2018-05-30 23:56:52 -0500

    Scopes and Closures: Chapter 5. Intro.

--- scopes-and-closures/examples.js
+++ scopes-and-closures/examples.js
@@ -1,15 +1,15 @@
 /*** Chapter 1: What is Scope? ***/
 // Function Scope, start looking for variables inside our function scope
 function multiply(a) {
-	let b = 2;
-	return a * b;
+  let b = 2;
+  return a * b;
 }
 
 console.log(multiply(3)); // 6
 
 // Nested Scope: Start looking 'b' from sumAPlusB function scope, moves up to global scope since it didn't find 'b'
 function sum(a) {
-	return a + b;
+  return a + b;
 }
 
 let b = 2;
@@ -19,13 +19,13 @@ console.log(sum(3)); // 5
 /*** Chapter 2: Lexical Scope ***/
 // Lexical Scope
 function one(a) {
-	let b = a * 2;
+  let b = a * 2;
 
-	function two(c) {
-		console.log({ a, b, c });
-	}
+  function two(c) {
+    console.log({ a, b, c });
+  }
 
-	two(b * 3);
+  two(b * 3);
 }
 
 one(2); // { a: 2, b: 4, c: 12 }
@@ -40,24 +40,24 @@ one(2); // { a: 2, b: 4, c: 12 }
 // Scope From Functions: All variables belong to the function, can be used and reused throughout the entire
 // function (even accessible at nested scopes).∫∫
 function displayInfo(name) {
-	let age = 25;
-	let gender = "M";
+  let age = 25;
+  let gender = "M";
 
-	function displayName() {
-		return name;
-	}
+  function displayName() {
+    return name;
+  }
 }
 // console.log(age, gender, name); // All three fail
 // displayName(); // ReferenceError: displayName is not defined
 
 // Hiding in Plain Scope: "hide" variables and functions by enclosing them in the scope of a function
 function converter(number) {
-	function multiplyNumber(number) {
-		return number * 2;
-	}
+  function multiplyNumber(number) {
+    return number * 2;
+  }
 
-	let b = 5 + multiplyNumber(number);
-	return b;
+  let b = 5 + multiplyNumber(number);
+  return b;
 }
 
 console.log(converter(2)); // 9
@@ -72,8 +72,8 @@ console.log(converter(2)); // 9
 let x = 2;
 
 (function executeThisNow() {
-	let x = 3;
-	console.log("x value from the executeThisNow function: ", x);
+  let x = 3;
+  console.log("x value from the executeThisNow function: ", x);
 })();
 
 console.log("x value from the global scope function: ", x);
@@ -83,12 +83,12 @@ console.log("x value from the global scope function: ", x);
 // Anonymous vs. Named
 // anonymous function expression
 setTimeout(function() {
-	console.log("Waiting for 1 second");
+  console.log("Waiting for 1 second");
 }, 1000);
 
 // named function expression
 setTimeout(function timeOutHandler() {
-	console.log("Waiting fot 1.5 seconds");
+  console.log("Waiting fot 1.5 seconds");
 }, 1500);
 
 // Note: inline function expressions are a good practice. Since the name can be used to stack trace, makes more readable/understandable code (self-documenting), can be refer to itseld. No drawbacks.
@@ -100,27 +100,26 @@ setTimeout(function timeOutHandler() {
   let y = 2;
   const PI = 3.14159265359;
 
-  if(true) {
+  if (true) {
     let z = 3;
     const PI = 3.1416;
 
-    console.log('Inside the if:', {x, y ,z});
-    console.log('Inside the if:', {PI});
+    console.log("Inside the if:", { x, y, z });
+    console.log("Inside the if:", { PI });
   }
 
   // console.log('Outside the if statement block::' {x, y ,z}); // ReferenceError: z is not defined, since it has been declared with 'let' keyword
   // console.log('Outside the if statement block:' {PI}); // ReferenceError: PI is not defined, since it has been declared with 'const' keyword
-
 })();
 
 /*** Chapter 4: Hoisting ***/
 // Declaration vs Assignment: The declaration, is processed during the compilation phase. The assignament is processed during the execution phase.
 // console.log(myName); ReferenceError: myName is not defined
-let myName = "Hector";  
+let myName = "Hector";
 
 // Declarations are "moved" from the flow of the code to the top of the code, this is what is called "Hoisting".
 
-// Function Declarations vs Function Expressions: 
+// Function Declarations vs Function Expressions:
 
 // Function declarations are hoisted!
 hoistedFunctionInc();
@@ -132,6 +131,53 @@ function hoistedFunctionInc() {
 // myNotHoistedFunction(); ReferenceError: myNotHoistedFunction is not defined
 let myNotHoistedFunction = function() {
   console.log("This will not work, right?");
-}
+};
 
 // NOTE: Functions declarations are hosited first, then variables declarations.
+
+/*** Chapter 5: Closures  ***/
+// Closure: when a function can remember and access its lexical scope even when the function is executing outside its lexical scope.
+function externalFunc() {
+  let fibboSum = 1 + 2 + 3 + 5 + 8 + 13;
+
+  function innerFunc() {
+    console.log(fibboSum); // 32
+  }
+
+  innerFunc();
+}
+
+externalFunc();
+// innerFunc() is referncing sum outside his lexical scope. But that's only a partial power of closures.
+
+function getTeamInformation() {
+  let team = "Indianapolis Colts";
+
+  function displayTeam() {
+    console.log(team);
+  }
+
+  return displayTeam;
+}
+
+let teamName = getTeamInformation();
+console.log(teamName()); // "Indianapolis Colts";
+
+// 1. The function displayTeam() has lexical scope access to the inner scope of getTeamInformation()
+// 2. Return as value the displayTeam() function itself (line 160)
+// 3. Execute getTeamInformation, assign the value returned (displayTeam() function) and assign it to teamName() (line 163)
+// 4. Invoke teamName(), that invokes displayTeam(), but by another identifier reference
+// 5. displayTeam() is executed, but is executed outside its lexical scope!
+
+// Note 1: Even if is expected that getTeamInformation() inner scope go away (garbage collector) it doesn't happen.
+// Note 2: Inner scope of getTeamInformation() still in use by displayTeam()
+// Note 3: displayTeam() has a reference on that scope, and that reference is called a 'closure'
+
+// Other Examples
+function message(note) {
+  setTimeout(() => {
+    console.log(note);
+  }, 2000);
+}
+
+message("Display me after 2 second.");    // Display me after 2 second.

1c28ef887527f4c48822e75c13fffac0c62abb9f
Hector Palomares <hector.palomares@alteumx.com>
2018-06-11 18:23:44 -0500

    Chapter 6. Comparing OO vs OLOO

--- this-object-properties/chapter-6.js
+++ this-object-properties/chapter-6.js
@@ -24,3 +24,74 @@ note.setContent(4, "Hello wonderful world!");
 console.log(note.getContent());                 // The note ID 4, and its contents are Hello wonderful world!
 
 // 'Task' and 'note' are not classes (or functions), both are only objects. 'note' object is set up via object.create(Task) to its [[Prototype]] delegate to Task.
+
+// Prototypal Inheritance Pattern (Object Oriented)
+// 1. Declare Building abstract class
+function Building (size, pricePerSquareFeet) {
+    this.size = size;
+    this.pricePerSquareFeet = pricePerSquareFeet;
+}
+
+// 2. Declare Building class methods
+Building.prototype.calculateCost = function () {
+    return "$" + (this.size * this.pricePerSquareFeet).toFixed(2);
+};
+
+// 3. Declare School abstract class
+function School(size, pricePerSquareFeet, type = "School") {
+    Building.call(this, size, pricePerSquareFeet, type);
+    this.type = type;
+}
+
+// 4. "Inherit" Building the methods 
+School.prototype = Object.create(Building.prototype);
+
+// 5. Declare School methods
+School.prototype.showType = function() {
+    return `This school is a ${this.type}`
+};
+
+let itesm = new School("100", 200, "Univeristy");
+let jfk = new School("75", 100, "Middle School");
+
+console.log(itesm.calculateCost());
+console.log(jfk.calculateCost());
+console.log(itesm.showType());
+console.log(jfk.showType());
+
+// Delegation Pattern (Object Linked to Other Object)
+// 1. Create object BuildingX with two properties as functions
+let BuildingX = {
+    init: function(size, pricePerSquareFeet) {
+        this.size = size;
+        this.pricePerSquareFeet = pricePerSquareFeet;
+    },
+    calculateCost: function() {
+        return "$" + (this.size * this.pricePerSquareFeet).toFixed(2) + "X";
+    }
+};
+
+// 2. Link SchoolX to BuildingX
+let SchoolX = Object.create(BuildingX);
+
+// 3. Add a create() method to the SchoolX object
+SchoolX.create = function(size, pricePerSquareFeet, type) {
+    this.init(size, pricePerSquareFeet);
+    this.type = type;
+}
+
+// 4. Add a showType() method to the SchoolX object
+SchoolX.showType = function() {
+    return `This school is a ${this.type}`
+};
+
+let itesmX = Object.create(SchoolX);
+itesmX.create("100", 200, "UniveristyX");
+
+let jfkX = Object.create(SchoolX);
+jfkX.create("75", 100, "Middle SchoolX");
+
+console.log(itesmX.calculateCost());
+console.log(jfkX.calculateCost());
+console.log(itesmX.showType());
+console.log(jfkX.showType());
